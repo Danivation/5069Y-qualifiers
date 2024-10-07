7e557cf9b92b722c6b62f61eeb784597a5223a17
Daniel Velasco <divelasco7@gmail.com>
2024-10-07 15:52:35 -0500

    minor commit

--- src/auton.cpp
+++ src/auton.cpp
@@ -76,36 +76,65 @@ void autonRedRight() {
 }
 
 void autonBlueLeft() {
-  // autonomous actions (blue left)
-  Inertial.setHeading(180, degrees);
-  drivePID(-340);
-  turnPID(215);
-  drivePID(-330);
+  // autonomous actions (new blue left)
+  Inertial.setHeading(0, degrees);
+  drivePID(-900);
   MobileGoalPiston.set(true);
-  turnPID(270);
   IntakeMotor.spin(forward);
-  drivePID(300);
-  wait(500, msec);
-  turnPID(45);
+  turnPID(90);
   drivePID(600);
+  wait(1500, msec);
+  IntakeMotor.stop();
+  MobileGoalPiston.set(false);
+  turnPID(0);
+  drivePID(-600);
+  MobileGoalPiston.set(true);
+  IntakeMotor.spin(forward);
+  turnPID(315);
+  drivePID(1697);
+  wait(500, msec);
+  IntakeMotor.spin(reverse);
+  wait(500, msec);
+  ArmPiston.set(true);
+  IntakeMotor.stop();
+  turnPID(0);
+  drivePID(600);
+  ArmPiston.set(false);
+  drivePID(-600);
+  turnPID(160);
+  drivePID(1800);
 }
 
 void autonBlueRight() {
-  // autonomous actions (blue right)
-  Inertial.setHeading(180, degrees);
-  drivePID(-165);
-  turnPID(145);
-  drivePID(-470);
+  // autonomous actions (new blue right)
+  Inertial.setHeading(0, degrees);
+  drivePID(-900);
   MobileGoalPiston.set(true);
-  turnPID(85);
   IntakeMotor.spin(forward);
-  drivePID(210);
-  wait(500, msec);
-  turnPID(20);
-  drivePID(220);
-  wait(500, msec);
-  turnPID(255);
+  turnPID(270);
   drivePID(600);
+  wait(500, msec);
+  turnPID(170);
+  drivePID(600);
+  wait(500, msec);
+  drivePID(-300);
+  turnPID(190);
+  drivePID(300);
+  wait(500, msec);
+  turnPID(45);
+  drivePID(1697);
+  MobileGoalPiston.set(false);
+  wait(500, msec);
+  IntakeMotor.spin(reverse);
+  wait(500, msec);
+  ArmPiston.set(true);
+  IntakeMotor.stop();
+  turnPID(0);
+  drivePID(600);
+  ArmPiston.set(false);
+  drivePID(-600);
+  turnPID(200);
+  drivePID(1800);
 }
 
 int AutonSelect() {
@@ -113,7 +142,10 @@ int AutonSelect() {
     Controller1.Screen.clearScreen();
     Controller1.Screen.setCursor(1, 1);
     Controller1.Screen.print("Auton: None");
+    printf("Screen updated\n");
     autonCallback = autonNone;
+    printf("Callback set to ");
+    printf(autonCallback);
     waitUntil(!AutonSelector.pressing());
     waitUntil(AutonSelector.pressing());
 
