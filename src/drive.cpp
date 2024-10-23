378ce7484f9845fb0e2e79dea5f3d112c648545c
Daniel Velasco <56180307+Danivation@users.noreply.github.com>
2024-10-23 11:43:30 -0500

    minor commit

--- src/drive.cpp
+++ src/drive.cpp
@@ -14,6 +14,7 @@ void drivePID(double targetDistance) {
   RMotorA.resetPosition();
   RMotorB.resetPosition();
   RMotorC.resetPosition();
+  wait(100, msec);
   //TrackerWheel.resetPosition();
   TimerDrive.clear();
 
